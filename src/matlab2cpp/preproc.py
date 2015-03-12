918df79332b93600719685d82071b08929ba31db
Jonathan Feinberg <jonathf@gmail.com>
2015-03-12 18:27:57 +0100

    removing \r again

--- src/matlab2cpp/preproc.py
+++ src/matlab2cpp/preproc.py
@@ -7,7 +7,7 @@ errors = set([])
 
 def prefix_hack(text):
 
-#      text = re.sub(r"\r", r"", text)         # windows to posix
+    text = re.sub(r"\r", r"", text)         # windows to posix
     text = re.sub(r"\.{3,}\n *", r" ", text)    # join "..."
     text = re.sub(r"[ \t]+", " ", text)         # spaces be reduced
     text = re.sub(r"((^|\n) | \n ?)+", r"\n", text)    # indenting be gone
