b108313f847ceba327289333c4c02057e97efccd
Jonathan Feinberg <jonathf@gmail.com>
2015-05-18 12:58:46 +0200

    floor handle for scalars

--- src/matlab2cpp/targets/reserved.py
+++ src/matlab2cpp/targets/reserved.py
@@ -394,6 +394,9 @@ def Get_floor(node):
     if node[0].mem > 1:
         node.type = (node[0].dim, 1)
 
+    if node.dim == 0:
+        return "std::floor(%(0)s)"
+
     return "arma::floor(%(0)s)"
 
 
