bb18258b8f7f974f095bf0bbae49f4a5e4b704cd
Jonathan Feinberg <jonathf@gmail.com>
2015-06-08 10:08:36 +0200

    Added common for assign for more fine grained error-handle

---
+++ src/matlab2cpp/targets/assign_common.py
@@ -0,0 +1,47 @@
+
+
+def Assign(node):
+
+    lhs, rhs = node
+    if "TYPE" in (lhs.type, rhs.type) or lhs.type == rhs.type:
+        return "%(0)s = %(1)s ;"
+
+    if lhs.num and rhs.num:
+
+        if (lhs.dim == 2 and rhs.dim == 1) or\
+                (lhs.dim == 1 and rhs.dim == 2):
+            out = "%(1)s.t()"
+        else:
+            out = "%(1)s"
+
+        if lhs.dim == 0 and rhs.dim == 0:
+
+            if lhs.mem >= rhs.mem:
+                out = "(" + lhs.type + ") " + out
+            else:
+                node.warning("Type reduction from %s to %s" %\
+                        (rhs.type, lhs.type))
+
+        elif lhs.dim in (1,2) and rhs.dim in (3, 4):
+            out = "arma::vectorise(" + out + ")"
+
+        elif lhs.dim > 0 and rhs.dim > 0:
+
+            if lhs.mem >= rhs.mem:
+                node.warning("Possible size incompatibility "+\
+                        "%s and %s" % (lhs.type, rhs.type))
+            else:
+                node.warning("Type reduction and possible size "+\
+                        "incompatible %s and %s" % (lhs.type, rhs.type))
+
+        else:
+            node.error("Types incompatible %s and %s" % (lhs.type, rhs.type))
+
+    else:
+        node.error("Types incompatible %s and %s" % (lhs.type, rhs.type))
+
+    out = "%(0)s = " + out + " ;"
+    return out
+
+
+
