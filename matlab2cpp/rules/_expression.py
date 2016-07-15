2d9073df456b74d6336961150341dbe9495a00d6
Geir Yngve Paulsen <geirpa@gmail.com>
2016-07-15 15:06:44 +0200

    Updated init test for c = a*b -> c = as_scalar(a*b) (a is rowvec, b vec)

--- matlab2cpp/rules/_expression.py
+++ matlab2cpp/rules/_expression.py
@@ -98,7 +98,7 @@ Examples:
     >>> print mc.qscript("a = [1,2,3]; b = [4;5;6]; c = a*b")
     a = {1, 2, 3} ;
     b = {4, 5, 6} ;
-    c = a*b ;
+    c = arma::as_scalar(a*b) ;
     """
 
     if not node[0].num:
