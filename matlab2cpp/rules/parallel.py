6a707afc93a8906b912adf7c9d6a976c6344e62b
Geir Yngve Paulsen <geirpa@gmail.com>
2016-10-05 15:14:14 +0200

    Changes in parallel code for tbb

--- matlab2cpp/rules/parallel.py
+++ matlab2cpp/rules/parallel.py
@@ -1,10 +1,10 @@
 
 def variable_lists(node):
-    nodes = node[2].flatten(ordered=False, reverse=False, inverse=False)
+    nodes = node.flatten(ordered=False, reverse=False, inverse=False)
 
     #store some variable names, in private or shared
-    private_variable = []
-    shared_variable = []
+    #private_variable = []
+    #shared_variable = []
     assigned_var = []
     type_info = []
 
@@ -12,12 +12,12 @@ def variable_lists(node):
     iterator_name = node[0].name
     for n in nodes:
         if n.cls == "Assign":
-            index = n.parent.children.index(n)
+            #index = n.parent.children.index(n)
 
             if n[0].cls == "Var":
                 assigned_var.append(n[0].name)
                 type_info.append(n[0].type)
-
+            """
             if n[0].cls == "Set":
                 var_name = n[0].name
 
@@ -30,38 +30,27 @@ def variable_lists(node):
                     if subnode.name and subnode.name == iterator_name:
                         shared_variable.append(var_name)
                         #print subnode.name
+            """
 
-    shared_variable = list(set(shared_variable))
+        if n.cls == "Var" and n.parent.cls == "For":
+            assigned_var.append(n.name)
+            type_info.append(n.type)
+
+    #shared_variable = list(set(shared_variable))
     #print shared_variable
 
-    for n in nodes:
-        if (n.cls == "Var" or n.cls == "Get") and n.backend != "reserved" and n.name \
-                not in [shared_variable, node[0].name]:
-            private_variable.append(n.name)
+    #for n in nodes:
+    #    if (n.cls == "Var" or n.cls == "Get") and n.backend != "reserved" and n.name \
+    #            not in [shared_variable, node[0].name]:
+    #        private_variable.append(n.name)
 
-    private_variable = list(set(private_variable))
-    #print private_variable
+    #private_variable = list(set(private_variable))
 
-    #combine shared and private variable to tbb [ , , , , ] with & before shared
-    #my_string = ''
-
-    #for var in private_variable:
-    #    my_string += var + ", "
-
-    #for var in shared_variable:
-    #    my_string += "&" + var + ", "
-
-    #my_string = my_string.rstrip(", ")
-    #my_string = "[" + my_string + "]"
-    #print my_string
-    #print "assigned var:\n"
-    #print assigned_var
-
-
-    return private_variable, shared_variable, assigned_var, type_info
+    #return private_variable, shared_variable, assigned_var, type_info
+    return assigned_var, type_info
 
 def omp(node, start, stop, step):
-    private_variable, shared_variable, assigned_var, type_info = variable_lists(node)
+    assigned_var, type_info = variable_lists(node)
 
     #out = "#pragma omp parallel for\nfor (%(0)s=" + start + \
             #    "; %(0)s<=" + stop + "; %(0)s"
@@ -76,27 +65,9 @@ def omp(node, start, stop, step):
     return out
 
 def tbb(node, start, stop, step):
-    private_variable, shared_variable, assigned_var, type_info = variable_lists(node)
-
-    #print "----type_info------"
-    #print type_info
+    assigned_var, type_info = variable_lists(node)
 
     out = "{\n"
-
-    #temp_list = []
-    #temp_assigned_var = set(assigned_var)
-
-    #for var in private_variable:
-    #    if var in temp_assigned_var:
-    #        temp_list.append("&_" + var)
-    #    else:
-    #        temp_list.append("&" + var)
-
-    #for var in shared_variable:
-    #    temp_list.append("&" + var)
-
-    #temp_str = ", ".join(temp_list)
-    #temp_str = "[" + temp_str + "]"
 
     for var, type in zip(assigned_var, type_info):
         out += "tbb::enumerable_thread_specific<" + type + "> " + "_" + var + " = " + var + " ;\n"
@@ -108,7 +79,7 @@ def tbb(node, start, stop, step):
     for var, type in zip(assigned_var, type_info):
         out += type + "& " + var + " = _" + var + ".local() ;\n"
 
-    out += "\nfor (" + node[0].type + " %(0)s = _range.begin(); %(0)s != _range.end(); %(0)s"
+    out += "\nfor (" + "%(0)s = _range.begin(); %(0)s != _range.end(); %(0)s"
 
     # special case for '+= 1'
     if step == "1":
