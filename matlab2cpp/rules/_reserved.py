9ec6f4ad5a9fa63923df3c937c8c7f89e8450ffe
Geir Yngve Paulsen <geirpa@gmail.com>
2017-03-22 16:28:15 +0100

    Added asin, sinh, asinh to reserved

--- matlab2cpp/rules/_reserved.py
+++ matlab2cpp/rules/_reserved.py
@@ -13,7 +13,7 @@ reserved = {
 "false", "true", "pi", "inf", "Inf", "nan", "NaN",
 "eps", "exp", "log", "log2", "log10", "power", "floor", "ceil",
 "cos", "acos", "cosh", "acosh",
-"sin", "mod",
+"sin", "asin", "sinh", "asinh", "mod",
 "eye", "flipud", "length", "max", "min", "size", "chol",
 "transpose", "ctranspose",
 "abs", "sqrt", "nextpow2", "fft", "ifft", "fft2", "ifft2", "hankel",
@@ -159,6 +159,24 @@ def Get_sin(node):
         node.include("cmath")
         return "std::sin(", ", ", ")"
     return "arma::sin(", ", ", ")"
+
+def Get_asin(node):
+    if node[0].dim == 0 and node[0].mem != 4:
+        node.include("cmath")
+        return "std::asin", ", ", ")"
+    return "arma::asin(", ", ", ")"
+
+def Get_sinh(node):
+    if node[0].dim == 0 and node[0].mem != 4:
+        node.include("cmath")
+        return "std::sinh", ", ", ")"
+    return "arma::sinh(", ", ", ")"
+
+def Get_asinh(node):
+    if node[0].dim == 0 and node[0].mem != 4:
+        node.include("cmath")
+        return "std::asinh", ", ", ")"
+    return "arma::asinh(", ", ", ")"
 
 # Special handle of 'i'-variable
 """ removed from reserved: "i",
