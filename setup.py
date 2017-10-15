9710e397beabdb7132d4d0bcb89fa9067578bdef
Jonathan Feinberg <jonathf@gmail.com>
2017-10-15 14:44:31 +0200

    tick 2.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="matlab2cpp",
-    version="2.0",
+    version="2.0.1",
     packages=find_packages("src"),
     package_dir={"": "src"},
     entry_points={"console_scripts": ["m2cpp = matlab2cpp:m2cpp"]},
